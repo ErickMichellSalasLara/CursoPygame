8951bb8fcef04cfee6087e9cd04f93145e3aacd3
ErickMichellSalasLara <alchilenoseman@gmail.com>
2026-04-02 18:45:05 -0600

    Add: Se agregaron aún más partes del curso

--- Disparando.py
+++ Disparando.py
@@ -1,80 +1,89 @@
 import pygame, random
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-
-class Meteor(pygame.sprite.Sprite): #Se crea la clase de la moneda, usando una subclase de Sprite
-    def __init__(self): #Iniciamos la clase y la super clase
-        super().__init__()
-        self.image = pygame.image.load("imagenes/meteor.png").convert_alpha() #Se carga la imagen
-        self.rect =  self.image.get_rect() # Posicionamiento del Sprite
-        # Creamos un encapsulamiento del movimiento de la moneda, para que el codigo sea mas ordenado y limpio
-        # Este encapsulamiento hace que la moneda se mueva hacia abajo, simulando que esta cayendo
-    def update(self):
-        self.rect.y += 1 
-        
-        if self.rect.y > 600: #Si la moneda sale de la pantalla, se vuelve a posicionar en un lugar random arriba de la pantalla
-            self.rect.y = -20
-            self.rect.x = random.randrange(900)
-            
-        
 class Player(pygame.sprite.Sprite):
-    def __init__(self): #Iniciamos la clase y la super clase
+    def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("imagenes/player.png").convert_alpha() #Se carga la imagen
-        self.rect =  self.image.get_rect() # Posicionamiento del Sprite
-        # Creamos un encapsulamiento del movimiento del jugador, para que el codigo sea mas ordenado y limpio
+        self.image = pygame.image.load("imagenes/player.png").convert_alpha()
+        self.rect = self.image.get_rect() # Posicionamiento del Sprite
+        
     def update(self):
         mouse_pos = pygame.mouse.get_pos()
         self.rect.x = mouse_pos[0]
-        self.rect.y = mouse_pos[1]
+        self.rect.y = 510
 
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("imagenes/meteor.png").convert_alpha()
+        self.rect = self.image.get_rect() # Posicionamiento del Sprite
+    
+class Laser(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("imagenes/laser.png").convert_alpha()
+        self.rect = self.image.get_rect() # Posicionamiento del Sprite
+    
+    def update(self):
+        self.rect.y -= 5 #Entre más alto el número, más rápido va el láser
 
-pygame.init()
+# Definir colores
+BLACK = (0, 0, 0) 
+WHITE = (255, 255, 255) 
 
-pygame.display.set_caption("Monedas cayendo") #Nombre de la ventana
-screen = pygame.display.set_mode([900,600])
-clock = pygame.time.Clock()
+pygame.init() #Aqui se inicializa la libreria de pygame
+size = (900, 600) #Aqui se define el tamaño de la pantalla
 
-done = False
+clock = pygame.time.Clock() #Aqui se crea un reloj para controlar los frames por segundo
+screen = pygame.display.set_mode(size) #Aqui se crea la ventana con el tamaño definido anteriormente
 
-background = pygame.image.load("imagenes/background.png").convert()
-
+done = False 
 score = 0
 
-''' Estas listas almacenan todos los Sprites que se creen '''
-all_sprite_list = pygame.sprite.Group()
-meteor_list = pygame.sprite.Group()
+laser_list = pygame.sprite.Group() #Se crea una lista para almacenar los láseres
+all_sprite_list = pygame.sprite.Group() #Se crea una lista para almacenar los sprites
+meteor_list = pygame.sprite.Group() #Se crea una lista para almacenar los meteoros
 
 for i in range(50):
-    meteorito = Meteor() #Se crean 50 instancias de Moneda
-    meteorito.rect.x = random.randrange(900) #Se posicionan en un lugar random
-    meteorito.rect.y = random.randrange(600)
-    
-    meteor_list.add(meteorito)
-    all_sprite_list.add(meteorito)
-    
-player = Player() #Se crea la variable del jugador y se iguala a la clase, la cual contiene las bases del jugador
-all_sprite_list.add(player) #Se añade el jugador a la lsta de todoos los sprites
+    meteor = Meteor()
+    meteor.rect.x = random.randrange(880) #Se posicionan en un lugar random
+    meteor.rect.y = random.randrange(450) #Se posicionan en un
 
-while not done:
+    meteor_list.add(meteor)
+    all_sprite_list.add(meteor)
+
+player = Player() #Se crea la instancia del jugador
+all_sprite_list.add(player) #Añadir el jugador a la lista de sprites
+
+while not done: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            done = True
-
-    all_sprite_list.update() #Llamamos al metodo update de todos los sprites  
-
-    meteor_hit_list = pygame.sprite.spritecollide(player, meteor_list, True) #Se detecta la colision entre el jugador y los meteoros, y se borra el meteorito al colisionar
-    
-    for meteorito in meteor_hit_list:
-        score += 1
-        print(score)
+            done = True 
+            
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        laser = Laser() #Se crea una instancia del láser
+        laser.rect.x = player.rect.x + 45 # Se posiciona el láser en la posición del jugador
+        laser.rect.y = player.rect.y - 20# Se posiciona el láser en la posición del jugador
         
+        all_sprite_list.add(laser) #Se añade el láser a la lista de sprites
+        laser_list.add(laser) #Se añade el láser a la lista de láseres
+                   
+    all_sprite_list.update() #Aqui se actualizan los sprites, en este caso solo el jugador
+            
     screen.fill(WHITE)
- 
-    all_sprite_list.draw(screen) #Con un solo metodo ya se puede llegar a crear todo
- 
+    all_sprite_list.draw(screen) #Aqui se dibujan todos los sprites en la pantalla
+    
+    for laser in laser_list:
+        meteor_hit_list = pygame.sprite.spritecollide(laser, meteor_list, True) #Se detecta la colision entre el láser y los meteoros, y se borra el meteoro al colisionar   
+        for meteor in meteor_hit_list:
+            all_sprite_list.remove(laser) #Se borra el láser al colisionar
+            laser_list.remove(laser) #Se borra el láser de la lista de láseres
+            score += 1
+            print(score)
+        if laser.rect.y < -10: #Si el láser sale de la pantalla, se borra
+            all_sprite_list.remove(laser) #Se borra el láser al salir de la pantalla
+            laser_list.remove(laser) #Se borra el láser de la lista de láseres
+    
     pygame.display.flip()
     clock.tick(60)
- 
+
 pygame.quit()
